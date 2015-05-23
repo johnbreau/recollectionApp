a58c3c6bcc8fea256b9b5523627f9ce165c570a0
John Breau <jbreau@Johns-i7-MBP.home>
2015-05-23 10:31:58 -0500

    EOD 5/23

--- app/modules/core/config/routes.js
+++ app/modules/core/config/routes.js
@@ -1,35 +1,87 @@
-'use strict';
 
 /**
- * @ngdoc object
- * @name core.config
- * @requires ng.$stateProvider
- * @requires ng.$urlRouterProvider
- * @description Defines the routes and other config within the core module
- */
-angular
-    .module('core')
-    .config(['$stateProvider',
-        '$urlRouterProvider',
-        function($stateProvider, $urlRouterProvider) {
+//  * @ngdoc object
+//  * @name core.config
+//  * @requires ng.$stateProvider
+//  * @requires ng.$urlRouterProvider
+//  * @description Defines the routes and other config within the core module
+//  */
+ angular.module('core').config(['$stateProvider','$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
 
-            $urlRouterProvider.otherwise('/');
+ $urlRouterProvider.otherwise('/');
 
-            /**
-             * @ngdoc event
-             * @name core.config.route
-             * @eventOf core.config
-             * @description
-             *
-             * Define routes and the associated paths
-             *
-             * - When the path is `'/'`, route to home
-             * */
+//             *
+//              * @ngdoc event
+//              * @name core.config.route
+//              * @eventOf core.config
+//              * @description
+//              *
+//              * Define routes and the associated paths
+//              *
+//              * - When the path is `'/'`, route to home
+//              * 
+
+
             $stateProvider
                 .state('home', {
                     url: '/',
                     templateUrl: 'modules/core/views/home.html',
                     controller: 'HomeController'
-                });
+                })
+
+                .state('dailyForm1', {
+                    url: '/dailyForm1',
+                    templateUrl: 'modules/core/views/dailyForm1.html'
+                })
+
         }
     ]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// original code from Yeoman generator angularjs-cordova...
+// /**
+//  * @ngdoc object
+//  * @name core.config
+//  * @requires ng.$stateProvider
+//  * @requires ng.$urlRouterProvider
+//  * @description Defines the routes and other config within the core module
+//  */
+// angular
+//     .module('core')
+//     .config(['$stateProvider',
+//         '$urlRouterProvider',
+//         function($stateProvider, $urlRouterProvider) {
+
+//             $urlRouterProvider.otherwise('/');
+
+//             *
+//              * @ngdoc event
+//              * @name core.config.route
+//              * @eventOf core.config
+//              * @description
+//              *
+//              * Define routes and the associated paths
+//              *
+//              * - When the path is `'/'`, route to home
+//              * 
+//             $stateProvider
+//                 .state('home', {
+//                     url: '/',
+//                     templateUrl: 'modules/core/views/home.html',
+//                     controller: 'HomeController'
+//                 });
+//         }
+//     ]);
